5bb4a32d6753ce30daf44765fd6512bf07620663
blaze1234-in <140904356+blaze1234-in@users.noreply.github.com>
2025-03-27 23:46:34 +0530

    Update whiteroom.py

--- whiteroom.py
+++ whiteroom.py
@@ -89,8 +89,13 @@ async def main():
     await app.run_polling()
 
 if __name__ == "__main__":
+    import sys
+
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
     try:
-        loop = asyncio.get_event_loop()
+        loop = asyncio.get_running_loop()
     except RuntimeError:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
